a87e0cf51e7b00fd0ab432185a333285e08b4522
GCGsauce <gattigcg1@gmail.com>
2020-03-17 11:24:56 -0400

    bubble sort and selection done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,27 +1,42 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+def selection_sort(list_to_sort):
+    x = 0
+    while(x < len(list_to_sort)): 
+        min = x
+        for i in range(x + 1, len(list_to_sort)):
+            #find minimum value and place it in the sorted sub-list
+            if(list_to_sort[i] < list_to_sort[min]):
+                min = i
 
+        if(min != x):
+            temp = list_to_sort[x]
+            list_to_sort[x] = list_to_sort[min]
+            list_to_sort[min] = temp
 
+        print(list_to_sort)
 
-        # TO-DO: swap
+        x += 1      
 
-
-
-
-    return arr
+    return list_to_sort
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(list_to_sort):
+    swap_occured = True #merely so the loop initiates
+    i = 0 
+    while(swap_occured):
+        swap_occured = False
+        for x in range(0, len(list_to_sort) - i - 1):
+            if(list_to_sort[x] > list_to_sort[x+1]):
+                temp = list_to_sort[x+1]
+                list_to_sort[x+1] = list_to_sort[x]
+                list_to_sort[x] = temp
+                swap_occured = True
 
-    return arr
+        print(list_to_sort)
+        i += 1
+
+    return list_to_sort
 
 
 # STRETCH: implement the Count Sort function below
